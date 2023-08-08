c3ec03c4a8d23f0195d748c4144668adf4c1316e
Elis Memic <elismemic5@gmail.com>
2023-08-08 13:53:10 +0200

    added javascript file with functions

---
+++ main.js
@@ -0,0 +1,72 @@
+let bookCollection = [];
+
+const displayBooks = () => {
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
+  bookCollection.forEach((book) => {
+    const bookContainer = document.createElement('article');
+    bookList.appendChild(bookContainer);
+    bookContainer.innerHTML = ''; // Clear the container
+
+    const titleElement = document.createElement('p');
+    titleElement.textContent = book.title;
+
+    const authorElement = document.createElement('p');
+    authorElement.textContent = book.author;
+
+    const buttonElement = document.createElement('button');
+    buttonElement.textContent = 'Remove';
+    buttonElement.setAttribute('data-id', book.id);
+    buttonElement.addEventListener('click', () => removeBook(book.id));
+
+    const hrElement = document.createElement('hr');
+
+    bookContainer.appendChild(titleElement);
+    bookContainer.appendChild(authorElement);
+    bookContainer.appendChild(buttonElement);
+    bookContainer.appendChild(hrElement);
+  });
+};
+
+const addBook = () => {
+  const titleInput = document.getElementById('title-input');
+  const authorInput = document.getElementById('author-input');
+  const id = Math.round(Date.now());
+
+  const title = titleInput.value;
+  const author = authorInput.value;
+
+  const newBook = { title, author, id };
+
+  bookCollection.push(newBook);
+
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+
+  // CLEARING INPUTS
+  titleInput.value = '';
+  authorInput.value = '';
+  displayBooks();
+};
+
+const removeBook = (id) => {
+  console.log('Removing book with id:', id);
+  bookCollection = bookCollection.filter((book) => book.id !== id);
+
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  displayBooks();
+};
+
+const addButton = document
+  .getElementById('add-book-button')
+  .addEventListener('click', addBook);
+
+window.addEventListener('load', () => {
+  const storedBookCollection = JSON.parse(
+    localStorage.getItem('bookCollection')
+  );
+
+  if (storedBookCollection) {
+    bookCollection.push(...storedBookCollection);
+  }
+  displayBooks();
+});
